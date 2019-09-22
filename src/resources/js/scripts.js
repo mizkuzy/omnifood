87c998c0869da34c879f012228e8de114b4f20e1
mizkuzy <Allright22>
2019-09-22 17:02:58 +0300

    add animation on scrolling

--- src/resources/js/scripts.js
+++ src/resources/js/scripts.js
@@ -55,6 +55,24 @@ $(document).ready(() => {
         }
       }
     });
+
+  /* Animations on scrolling*/
+  $('.js--wp-1').waypoint(() => {
+    $('.js--wp-1').addClass('animated fadeIn')
+  }, {offset: '50%'})
+
+  $('.js--wp-2').waypoint(() => {
+    $('.js--wp-2').addClass('animated fadeInUp')
+  }, {offset: '50%'})
+
+  $('.js--wp-3').waypoint(() => {
+    $('.js--wp-3').addClass('animated fadeIn')
+  }, {offset: '50%'})
+
+  $('.js--wp-4').waypoint(() => {
+    $('.js--wp-4').addClass('animated pulse')
+  }, {offset: '50%'})
+
 })
 
 // console.log('hi')
